ffe5e82d07e0ee410e4351b7e4c8162b9665a4b4
liuwl <liuwl@bytter.com>
2017-01-12 09:31:47 +0800

    task-151

---
+++ src/main/java/com/betterjr/modules/wechat/handler/url/WorkFlowUrlHandler.java
@@ -0,0 +1,27 @@
+// Copyright (c) 2014-2017 Bytter. All rights reserved.
+// ============================================================================
+// CURRENT VERSION 
+// ============================================================================
+// CHANGE LOG
+// V2.0 : 2017年1月10日, liuwl, creation
+// ============================================================================
+package com.betterjr.modules.wechat.handler.url;
+
+import com.betterjr.modules.wechat.dispatcher.UrlControl;
+
+/**
+ * @author liuwl
+ *
+ */
+public class WorkFlowUrlHandler implements UrlHandler {
+
+    /* (non-Javadoc)
+     * @see com.betterjr.modules.wechat.handler.url.UrlHandler#handle(java.lang.String, com.betterjr.modules.wechat.dispatcher.UrlControl)
+     */
+    @Override
+    public void handle(String anState, UrlControl anUrlControl) throws Exception {
+        // TODO Auto-generated method stub
+
+    }
+
+}
